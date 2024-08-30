4d3339ee8cb3b6e0c1e96be2a117012f75b3eb59
Mashhad Ali Shah <165510733+MASHHADALI@users.noreply.github.com>
2024-08-30 11:55:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// SLICE used to remove data
+// 0 4 ka matlab zero sai 4 tak show krao aur 5 ko remove krao
+// the first digit is length which starts from 0
+// let num = ["1","2","3","4","5"]
+
+// let cutnum = num.slice(0,4)
+// console.log(cutnum)
+
+
+
+
+// SPLICE  used to replace data
+// the first 2 is the number while the second 2 sai agay jo 2 number hai usai replace krai
+// let num = ["1", "2", "3" ,"4" ,"5"]
+// num.splice(2,2, "8","9")
+// console.log(num)
+
+
+//  FIRST LETTER BECOMES CAP
+// let name = ("nasir")
+// let capitalized = name[0].toUpperCase() + name.slice(1);
+// console.log(capitalized)
+
+
+
+// let name =prompt("enter your name")
+// let myname=name[0].toUpperCase() + name.slice(1);
+// console.log(myname)
+
+
+
